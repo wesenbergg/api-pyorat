d3a07872740c60c283d8035295c3673ac0120fdf
wesenbergg <b.b.j.1998@gmail.com>
2020-08-30 21:03:49 +0300

    -m

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react'
 import './App.css';
 
-function App() {
+const App = () => {
+  const [data, setData] = useState([])
+
+  useEffect( () => {
+    fetch('https://services1.arcgis.com/sswNXkUiRoWtrx0t/arcgis/rest/services/liipi_pyora_fasiliteetit/FeatureServer/0/query?where=HSL_ALUE%20%3D%20\'0\'&outFields=PYORAKAA,PAIKKOJA,TUNNUSFI,HSL_ALUE&outSR=4326&f=json')
+    .then(res => res.json())
+    .then(res => setData(res.features) )
+  }, [])
+  
+  const listBikes = () => data.map(e => 
+    <tr key={e.attributes.TUNNUSFI}>
+      <td>{e.attributes.TUNNUSFI}</td>
+      <td>{e.attributes.PYORAKAA}</td>
+      <td>{e.attributes.PAIKKOJA}</td>
+    </tr>
+    )
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <table className="table table-striped table-sm">
+          <thead>
+            <tr>
+              <th>Nimi</th>
+              <th>Pyöriä saatavilla</th>
+              <th>Paikkoja vapaana</th>
+            </tr>
+          </thead>
+          <tbody>
+            {listBikes()}
+          </tbody>
+        </table>
+      </div>
+  )
 }
 
 export default App;
