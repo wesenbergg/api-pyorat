f1e32a4bb2521e8e4497eb47b2a0651d6d2d5eb7
wesenbergg <b.b.j.1998@gmail.com>
2020-08-30 21:24:53 +0300

    Updates

---
+++ precache-manifest.f884c1e2e2ab4fdf9067d8bae857a7b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab6be2fb9deffbc6cfcd25eaffce226b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5081286845d410666a88",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "42e46dc36e2872a94309",
+    "url": "/static/js/2.3a523bc7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3a523bc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5081286845d410666a88",
+    "url": "/static/js/main.e4912301.chunk.js"
+  },
+  {
+    "revision": "a9c5df7f0e96da6d02ee",
+    "url": "/static/js/runtime-main.1dea4ba9.js"
+  }
+]);
